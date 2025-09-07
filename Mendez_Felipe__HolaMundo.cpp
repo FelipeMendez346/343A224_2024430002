c431c2b390f70f4d75f0e3ddf15452142494e800
FelipeMendez346 <felipemendez343@gmail.com>
2025-09-07 13:32:07 -0300

    Delete Mendez_Felipe__HolaMundo.cpp
    sda

--- Mendez_Felipe__HolaMundo.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout<<"Hola Mundo"<<endl;//Print:Hola Mundo en la terminal
-    return 0;//retorna para ver que se ejecuto correctamente el codigo
-}
